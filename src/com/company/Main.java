8a8a4e397bcd3dbf0d6adea75f5708ab58f079cc
TimofeyShved <timofeya123@inbox.ru>
2023-12-26 21:47:50 +0600

    locale bunle

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,16 @@
 package com.company;
 
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
 	    // Локализация
         Locale locale1 = new Locale("en", "US");
         Locale locale2 = Locale.FRANCE;
@@ -32,5 +35,16 @@ public class Main {
         System.out.println(DateFormat.getDateInstance(DateFormat.FULL, locale1).format(new Date()));
         System.out.println(DateFormat.getDateInstance(DateFormat.FULL, locale2).format(new Date()));
         System.out.println(DateFormat.getDateInstance(DateFormat.FULL, locale3).format(new Date()));
+
+        // выбор значений от региона
+        ResourceBundle bundle1 = ResourceBundle.getBundle("textLocal", new Locale("en", "US"));
+        ResourceBundle bundle2 = ResourceBundle.getBundle("textLocal", locale2);
+        ResourceBundle bundle3 = ResourceBundle.getBundle("textLocal", locale3);
+
+        System.out.println(bundle1.getString("value"));
+        System.out.println(bundle2.getString("value"));
+        System.out.println(bundle3.getString("value"));
+        // ?? - исправим:
+        System.out.println(new String(bundle3.getString("value").getBytes("ISO-8859-1"), "UTF-8"));
     }
 }
